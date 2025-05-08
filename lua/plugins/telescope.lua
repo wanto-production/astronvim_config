644ec286df0c3373203bc259e3584486ee5ab21e
wanto-production <ikwansatria3974@gmail.com>
2025-05-09 06:01:07 +0700

    update commit

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,8 +20,18 @@ return {
       local builtin = require "telescope.builtin"
       vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
     end,
+  },
+
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    cond = vim.fn.executable "make" == 1,
+    config = function() require("telescope").load_extension "fzf" end,
+  },
+
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function() require("telescope").load_extension "ui-select" end,
   },
 }
