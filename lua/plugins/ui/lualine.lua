b51f607126e50541fd7ed3401da75fbc1974a095
wanto-production <ikwansatria3974@gmail.com>
2025-05-10 16:54:27 +0700

    update commit

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -7,27 +7,26 @@ return {
       require("lualine").setup {
         options = {
           icons_enabled = true,
-          theme = "auto",
+          theme = "auto", -- mengikuti colorscheme
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
+          disabled_filetypes = { "NvimTree", "packer" },
           always_divide_middle = true,
-          always_show_tabline = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-          },
         },
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
+          lualine_c = {
+            {
+              "filename",
+              path = 1, -- 0 = hanya nama file, 1 = relative path, 2 = absolute path
+              symbols = {
+                modified = "●", -- file telah dimodifikasi
+                readonly = "",
+                unnamed = "[No Name]",
+              },
+            },
+          },
           lualine_x = { "encoding", "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
@@ -40,10 +39,7 @@ return {
           lualine_y = {},
           lualine_z = {},
         },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
+        extensions = { "nvim-tree", "fugitive", "lazy" },
       }
     end,
   },
